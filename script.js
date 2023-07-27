0afec0ac7a1662e88ac5dc584893ff6bf1eb15d9
REVANTH <rockierevanth@gmail.com>
2023-07-27 23:09:28 +0530

    UI-frame-1.00

--- script.js
+++ script.js
@@ -1,47 +1,82 @@
 //function to randomly select any of the three options
-function getComputerChoice(){
+function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
-    switch(choice){
+    switch (choice) {
         case 0:
             return 'rock';
-        break;
+            break;
 
         case 1:
-          return 'paper';
-        break;
+            return 'paper';
+            break;
 
         case 2:
-           return 'scissors';
-        break;
+            return 'scissors';
+            break;
 
         default:
             getComputerChoice();
-        break;
+            break;
     };
 }
 
+const draw = undefined;
+const lost = undefined;
+const won = undefined;
 
 // takes two inputs compare and result the winner!!
-function playRound(playerSelection, computerSelection){
-    
-    if(playerSelection === computerSelection){
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
         console.log('Draw try again!');
-    }else if (playerSelection === 'rock' && computerSelection === 'paper'){
+        return draw;
+
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         console.log('You Lose! Paper beats Rock');
-    }else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+        return lost;
+
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         console.log('You Lose! scissors beats Rock');
-    }else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+        return lost;
+
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         console.log('You Lose! Rock beats Scissors');
-    }else {
+        return lost;
+
+    } else {
         console.log('Hooray you WIN! u fucking bastard');
+        return won;
     }
 
 
 }
 
-const playerSelection = prompt("Type any one of these 1.rock , 2.paper, 3.scissors: ");
-console.log(`player: ${playerSelection} `);
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-// console.log(playRound(playerSelection, computerSelection));
-playRound(playerSelection, computerSelection);
+let playerSelection = prompt("Type any one of these 1.rock , 2.paper, 3.scissors: ");
+
+let computerSelection = getComputerChoice();
+
+
+
+
+// function to check winner after 5 rounds.. if 3 rounds are won the user is the winner.. else you know what (the user is the loser);
+function game(func, n) {
+    let playerWins = 0;
+    for (let i = 0; i < n; i++) {
+        playerSelection = prompt("Type any one of these 1.rock , 2.paper, 3.scissors: ");
+        console.log(`player: ${playerSelection}`);
+        computerSelection = getComputerChoice();
+        console.log(computerSelection);
+        let result = playRound(playerSelection, computerSelection);
+        if(result === won){
+            playerWins++
+        }
+
+    }
+    if(playerWins >= 3){
+        console.log("Congratulatios! mofo u did it");
+    }else{
+        console.log('U can\'t even win a goddamn rock-paper-game.U are a failure' );
+    }
+}
+
+game(playRound, 5);
