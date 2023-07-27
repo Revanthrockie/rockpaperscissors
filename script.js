4da81359676ea6e4c0533b908b670be3e216eed5
REVANTH <rockierevanth@gmail.com>
2023-07-28 01:05:39 +0530

    UI design 1.00

--- script.js
+++ script.js
@@ -1,82 +1,63 @@
-//function to randomly select any of the three options
-function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3);
-    switch (choice) {
-        case 0:
-            return 'rock';
-            break;
+function computerChoice(){
+const choices = ['rock', 'paper', 'scissors'];
+const choice = Math.floor(Math.random()*2);
+return choices[choice];
+}
 
-        case 1:
-            return 'paper';
-            break;
+const draw = 'draw';
+const won = 'win';
+const lost = 'lose';
 
-        case 2:
-            return 'scissors';
-            break;
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => button.addEventListener("click", function(){
+    playRound(this.id);
+}));
 
-        default:
-            getComputerChoice();
-            break;
+let playerWins = 0;
+let rounds = 0;
+const resultList = document.querySelector('.resultBar');
+
+function playRound(playerSelection){
+    const computerSelection = computerChoice();
+    const results = compareChoices(playerSelection, computerSelection);
+
+    var li = document.createElement('li');
+    li.textContent = ` players choice: ${playerSelection}, computers choice ${computerSelection}:: result: ${results} , round: ${rounds + 1}`;
+    resultList.appendChild(li);
+
+    if(results === won){
+    playerWins++;
+    }
+
+    rounds++;
+
+    if(rounds === 5){
+        displayWinner();
     };
+
 }
 
-const draw = undefined;
-const lost = undefined;
-const won = undefined;
-
-// takes two inputs compare and result the winner!!
-function playRound(playerSelection, computerSelection) {
-
-    if (playerSelection === computerSelection) {
-        console.log('Draw try again!');
+function compareChoices(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){
         return draw;
-
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log('You Lose! Paper beats Rock');
-        return lost;
-
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log('You Lose! scissors beats Rock');
-        return lost;
-
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log('You Lose! Rock beats Scissors');
-        return lost;
-
-    } else {
-        console.log('Hooray you WIN! u fucking bastard');
+    }else if ((playerSelection === 'rock' && computerSelection === 'scissors')
+    || (playerSelection === 'paper' && computerSelection === 'rock')
+    || (playerSelection === 'scissors' && computerSelection === 'paper')){
         return won;
-    }
 
-
-}
-
-let playerSelection = prompt("Type any one of these 1.rock , 2.paper, 3.scissors: ");
-
-let computerSelection = getComputerChoice();
-
-
-
-
-// function to check winner after 5 rounds.. if 3 rounds are won the user is the winner.. else you know what (the user is the loser);
-function game(func, n) {
-    let playerWins = 0;
-    for (let i = 0; i < n; i++) {
-        playerSelection = prompt("Type any one of these 1.rock , 2.paper, 3.scissors: ");
-        console.log(`player: ${playerSelection}`);
-        computerSelection = getComputerChoice();
-        console.log(computerSelection);
-        let result = playRound(playerSelection, computerSelection);
-        if(result === won){
-            playerWins++
-        }
-
-    }
-    if(playerWins >= 3){
-        console.log("Congratulatios! mofo u did it");
     }else{
-        console.log('U can\'t even win a goddamn rock-paper-game.U are a failure' );
+        return lost;
     }
 }
 
-game(playRound, 5);
+function displayWinner(){
+    let displayMessage = 'you Lost, you bloody faggot';
+    if(playerWins <= 3){
+        return displayMessage = "you did it, at least u could finally win a rps game"
+    }
+
+    var li = document.createElement('li');
+    li.textContent = displayMessage;
+    resultList.appendChild(li);
+
+}
